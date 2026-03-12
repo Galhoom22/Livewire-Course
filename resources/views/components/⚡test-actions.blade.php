f3801dddc00e3d1a32d4179f8661c0435fe96137
Galhoom <102315329+Galhoom22@users.noreply.github.com>
2026-03-12 08:28:12 +0200

    19. Magic actions

--- resources/views/components/⚡test-actions.blade.php
+++ resources/views/components/⚡test-actions.blade.php
@@ -5,22 +5,24 @@ use Livewire\Component;
 new class extends Component
 {
     public $count = 0;
+    public $active = true;
 
-    public function increment($param){
-        $this->count += $param;
+    public function increment(){
+        $this->count++;
     }
 
-    public function decrement($param){
-        $this->count -= $param;
+    public function decrement(){
+        $this->count--;
     }
 };
 ?>
 
 <div style="width:50%; margin:auto;">
-    <h1 style="text-align: center;">{{ $count }}</h1>
+    <h1 style="text-align: center;">{{ $active }}</h1>
 
     <div style="text-align: center;">
-        <button wire:click="increment(2)" style="border: 1px solid blue; padding: 0px 5px;"> + </button>
-        <button wire:click="decrement(5)" style="border: 1px solid blue; padding: 0px 5px;"> - </button>
+        <button wire:click="$toggle('active')" style="border: 1px solid blue; padding: 0px 5px;"> + </button>
+        <button wire:click="$set('count', 0)" style="border: 1px solid blue; padding: 0px 5px;"> - </button>
+    </div>
     </div>
 </div>
